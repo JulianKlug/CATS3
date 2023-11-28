04736fb528251ee38f0ddc50dffe15d01006bbc3
Julian Klug <tensu.wave@gmail.com>
2023-11-28 23:01:18 +0100

    Train cross val over 5 splits

--- training/train_crn.py
+++ training/train_crn.py
@@ -9,7 +9,9 @@ from data_loaders.temporAI_dataloader import load_gsu_dataset
 from utils import ensure_dir
 
 
-def train_crn(splits_folder_path: str, epochs: int, output_dir: str, verbose: bool = False):
+def train_crn(splits_folder_path: str, epochs: int, output_dir: str,
+              n_splits: int = 5,
+              verbose: bool = False):
     """
     Train a Counterfactual Regression Network (CRN) model.
 
@@ -66,38 +68,38 @@ def train_crn(splits_folder_path: str, epochs: int, output_dir: str, verbose: bo
     with open(os.path.join(output_dir, 'args.json'), 'w') as f:
         json.dump(args, f)
 
-    split = 0
-    data_path = os.path.join(splits_folder_path, 'train_features_split_{}.csv'.format(split))
-    continuous_outcomes_path = os.path.join(splits_folder_path, 'train_continuous_outcomes_split_{}.csv'.format(split))
-    gsu_dataset = load_gsu_dataset(data_path, continuous_outcomes_path)
+    for split in range(n_splits):
+        data_path = os.path.join(splits_folder_path, 'train_features_split_{}.csv'.format(split))
+        continuous_outcomes_path = os.path.join(splits_folder_path, 'train_continuous_outcomes_split_{}.csv'.format(split))
+        gsu_dataset = load_gsu_dataset(data_path, continuous_outcomes_path)
 
-    model = plugin_loader.get("treatments.temporal.regression.crn_regressor", epochs=params['epochs'],
-                              encoder_rnn_type=params['encoder_rnn_type'],
-                              encoder_hidden_size=params['encoder_hidden_size'],
-                              encoder_num_layers=params['encoder_num_layers'], encoder_bias=params['encoder_bias'],
-                              encoder_dropout=params['encoder_dropout'],
-                              encoder_bidirectional=params['encoder_bidirectional'],
-                              encoder_nonlinearity=params['encoder_nonlinearity'],
-                              encoder_proj_size=params['encoder_proj_size'],
-                              decoder_rnn_type=params['decoder_rnn_type'],
-                              decoder_hidden_size=params['decoder_hidden_size'],
-                              decoder_num_layers=params['decoder_num_layers'], decoder_bias=params['decoder_bias'],
-                              decoder_dropout=params['decoder_dropout'],
-                              decoder_bidirectional=params['decoder_bidirectional'],
-                              decoder_nonlinearity=params['decoder_nonlinearity'],
-                              decoder_proj_size=params['decoder_proj_size'],
-                              adapter_hidden_dims=params['adapter_hidden_dims'],
-                              adapter_out_activation=params['adapter_out_activation'],
-                              predictor_hidden_dims=params['predictor_hidden_dims'],
-                              predictor_out_activation=params['predictor_out_activation'], max_len=params['max_len'],
-                              optimizer_str=params['optimizer_str'], optimizer_kwargs=params['optimizer_kwargs'],
-                              batch_size=params['batch_size'], padding_indicator=params['padding_indicator']
-                              )
+        model = plugin_loader.get("treatments.temporal.regression.crn_regressor", epochs=params['epochs'],
+                                  encoder_rnn_type=params['encoder_rnn_type'],
+                                  encoder_hidden_size=params['encoder_hidden_size'],
+                                  encoder_num_layers=params['encoder_num_layers'], encoder_bias=params['encoder_bias'],
+                                  encoder_dropout=params['encoder_dropout'],
+                                  encoder_bidirectional=params['encoder_bidirectional'],
+                                  encoder_nonlinearity=params['encoder_nonlinearity'],
+                                  encoder_proj_size=params['encoder_proj_size'],
+                                  decoder_rnn_type=params['decoder_rnn_type'],
+                                  decoder_hidden_size=params['decoder_hidden_size'],
+                                  decoder_num_layers=params['decoder_num_layers'], decoder_bias=params['decoder_bias'],
+                                  decoder_dropout=params['decoder_dropout'],
+                                  decoder_bidirectional=params['decoder_bidirectional'],
+                                  decoder_nonlinearity=params['decoder_nonlinearity'],
+                                  decoder_proj_size=params['decoder_proj_size'],
+                                  adapter_hidden_dims=params['adapter_hidden_dims'],
+                                  adapter_out_activation=params['adapter_out_activation'],
+                                  predictor_hidden_dims=params['predictor_hidden_dims'],
+                                  predictor_out_activation=params['predictor_out_activation'], max_len=params['max_len'],
+                                  optimizer_str=params['optimizer_str'], optimizer_kwargs=params['optimizer_kwargs'],
+                                  batch_size=params['batch_size'], padding_indicator=params['padding_indicator']
+                                  )
 
-    # Train the model:
-    model.fit(gsu_dataset)
+        # Train the model:
+        model.fit(gsu_dataset)
 
-    save_to_file(os.path.join(output_dir, f'crn_model_{timestamp}_split_{split}.cpkl'), model)
+        save_to_file(os.path.join(output_dir, f'crn_model_{timestamp}_split_{split}.cpkl'), model)
 
     return None
 
@@ -109,10 +111,12 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--splits_folder_path', type=str, required=True)
     parser.add_argument('-e', '--epochs', type=int, required=True)
     parser.add_argument('-o', '--output_dir', type=str, required=True)
+    parser.add_argument('-n', '--n_splits', type=int, required=False, default=5)
     parser.add_argument('-v', '--verbose', type=bool, required=False, default=True)
     args = parser.parse_args()
 
     train_crn(splits_folder_path=args.splits_folder_path,
               epochs=args.epochs,
               output_dir=args.output_dir,
+              n_splits=args.n_splits,
               verbose=args.verbose)
